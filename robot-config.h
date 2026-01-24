79ef55b2ec44a7d5b3a255b7beb4a72d3d8e8029
tqm-vrc <tunishq@googlemail.com>
2026-01-24 09:44:20 +0000

    Create robot-config.h

---
+++ robot-config.h
@@ -0,0 +1,26 @@
+using namespace vex;
+
+extern brain Brain;
+
+extern motor LeftMotor;
+extern motor LeftMotor2;
+extern motor LeftMotor3;
+
+extern motor RightMotor;
+extern motor RightMotor2;
+extern motor RightMotor3;
+extern inertial Inertial17;
+
+extern motor Intake;
+extern motor Shooter;
+
+extern controller Controller;
+extern competition Competition;
+
+extern digital_out Descore;
+extern digital_out Scraper;
+extern digital_out MidDescore;
+
+
+
+void  vexcodeInit( void );
